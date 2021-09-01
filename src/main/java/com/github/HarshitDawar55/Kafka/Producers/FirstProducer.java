f2b3a65794a5045ad03b74fb4ce064dd3e5fe9c6
HarshitDawar55 <harshit.dawar55@gmail.com>
2021-09-02 01:34:33 +0530

    Simple Producer Code

--- src/main/java/com/github/HarshitDawar55/Kafka/Producers/FirstProducer.java
+++ src/main/java/com/github/HarshitDawar55/Kafka/Producers/FirstProducer.java
@@ -1,2 +1,35 @@
-package com.github.HarshitDawar55.Kafka.Producers;public class FirstProducer {
+package com.github.HarshitDawar55.Kafka.Producers;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class FirstProducer {
+    public static void main(String[] args) {
+        // Creating Required Variables
+        String BootStrap_Servers = "localhost:9092";
+
+        // Setting Producer Required Properties using the best way instead of old way by using the exact names in strings
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BootStrap_Servers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Creating Kafka Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        // Creating a Producer Record to send!
+        ProducerRecord<String, String> record =
+                new ProducerRecord<String, String>("First-Topic", "First Message from the Producer!");
+
+        // Sending the Data
+        producer.send(record);
+
+        // Flushing the Data & then closing the Producer!
+        producer.flush();
+        producer.close();
+    }
 }
